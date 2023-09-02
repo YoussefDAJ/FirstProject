de459e7625e04dedc56e70ebc94988db3fafe383
Youssef <youssefelbaye@gmail.com>
2023-09-02 17:02:56 +0200

    the main code

---
+++ YoutubeNameGenerator.py
@@ -0,0 +1,3 @@
+user_name = input("What's your NickName? : \n")
+Channel_subject = input("What's your channel about? \n")
+print("You can name your channel: " + Channel_subject + " with " + user_name)
